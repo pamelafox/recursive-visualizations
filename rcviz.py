75f4d1c28542f361ef7bbc0af02d73b55c9fefee
Pamela Fox <pamela.fox@gmail.com>
2022-05-19 22:00:11 -0700

    After black formatting

--- rcviz.py
+++ rcviz.py
@@ -9,21 +9,24 @@ from typing import Dict, List, Any
 
 import pydot
 
+
 class TooManyFramesError(Exception):
     pass
+
 
 class TooMuchTimeError(Exception):
     pass
 
+
 class callgraph(object):
-    '''singleton class that stores global graph data
-       draw graph using pygraphviz
-    '''
+    """singleton class that stores global graph data
+    draw graph using pygraphviz
+    """
 
     _callers: Dict[int, Any] = {}  # caller_fn_id : node_data
     _counter = 1  # track call order
     _unwindcounter = 1  # track unwind order
-    _step = 1 # track overall steps
+    _step = 1  # track overall steps
     _frames: List[int] = []  # keep frame objects reference
 
     @staticmethod
@@ -59,58 +62,81 @@ class callgraph(object):
     @staticmethod
     def get_frames():
         return callgraph._frames
-    
+
     @staticmethod
     def render():
-      dotgraph = pydot.Dot("rc-graph", graph_type="digraph", strict=False)
-      
-      # Create nodes
-      for frame_id, node in callgraph._callers.items():
-        aux_str = ''
-        for param, val in node.auxdata.items():
-          aux_str += " | %s: %s" % (param, val)
-        label = "{ %s(%s) %s}" % (
-                    node.fn_name, node.argstr(), aux_str)
-        dotgraph.add_node(pydot.Node(frame_id, label=label, shape='Mrecord'))
-      
-      # Create edges
-      for frame_id, node in callgraph._callers.items():
-        child_nodes = []
-        for child_id, counter, unwind_counter in node.child_methods:
-          child_nodes.append(child_id)
-          label = "(#%s)" % (counter)
-          dotgraph.add_edge(pydot.Edge(frame_id, child_id, color="black", label=label))
-          
-        # Order edges left to right
-        if len(child_nodes) > 1:
-          subgraph = pydot.Subgraph(rank='same')
-          prev_node = None
-          for child_node in child_nodes:
-            subgraph.add_node(pydot.Node(child_node))
-            if prev_node:
-              subgraph.add_edge(pydot.Edge(prev_node, child_node))
-            prev_node = child_node
-          dotgraph.add_subgraph(subgraph)
-      
-      parent_frame = None
-      for frame_id, node in callgraph._callers.items():
-        for child_id, counter, unwind_counter in node.child_methods:
-          child_node = callgraph._callers.get(child_id)
-          if child_node and child_node.ret is not None:
-            ret_label = f'{child_node.ret} (#{child_node.ret_step})'
-            dotgraph.add_edge(pydot.Edge(frame_id, child_id, dir="back", label=ret_label, color="green", headport="c"))
-        if parent_frame is None:
-          parent_frame = frame_id
-          if node.ret is not None:
-            ret_label = f'{node.ret} (#{node.ret_step})'
-            dotgraph.add_node(pydot.Node(99999999, shape='Mrecord', label='Result'))
-            dotgraph.add_edge(pydot.Edge(99999999, frame_id, dir="back", label=ret_label, color="Green", headport="c"))
-         
-      return dotgraph.to_string() 
+        dotgraph = pydot.Dot("rc-graph", graph_type="digraph", strict=False)
+
+        # Create nodes
+        for frame_id, node in callgraph._callers.items():
+            aux_str = ""
+            for param, val in node.auxdata.items():
+                aux_str += " | %s: %s" % (param, val)
+            label = "{ %s(%s) %s}" % (node.fn_name, node.argstr(), aux_str)
+            dotgraph.add_node(pydot.Node(frame_id, label=label, shape="Mrecord"))
+
+        # Create edges
+        for frame_id, node in callgraph._callers.items():
+            child_nodes = []
+            for child_id, counter, unwind_counter in node.child_methods:
+                child_nodes.append(child_id)
+                label = "(#%s)" % (counter)
+                dotgraph.add_edge(
+                    pydot.Edge(frame_id, child_id, color="black", label=label)
+                )
+
+            # Order edges left to right
+            if len(child_nodes) > 1:
+                subgraph = pydot.Subgraph(rank="same")
+                prev_node = None
+                for child_node in child_nodes:
+                    subgraph.add_node(pydot.Node(child_node))
+                    if prev_node:
+                        subgraph.add_edge(pydot.Edge(prev_node, child_node))
+                    prev_node = child_node
+                dotgraph.add_subgraph(subgraph)
+
+        parent_frame = None
+        for frame_id, node in callgraph._callers.items():
+            for child_id, counter, unwind_counter in node.child_methods:
+                child_node = callgraph._callers.get(child_id)
+                if child_node and child_node.ret is not None:
+                    ret_label = f"{child_node.ret} (#{child_node.ret_step})"
+                    dotgraph.add_edge(
+                        pydot.Edge(
+                            frame_id,
+                            child_id,
+                            dir="back",
+                            label=ret_label,
+                            color="green",
+                            headport="c",
+                        )
+                    )
+            if parent_frame is None:
+                parent_frame = frame_id
+                if node.ret is not None:
+                    ret_label = f"{node.ret} (#{node.ret_step})"
+                    dotgraph.add_node(
+                        pydot.Node(99999999, shape="Mrecord", label="Result")
+                    )
+                    dotgraph.add_edge(
+                        pydot.Edge(
+                            99999999,
+                            frame_id,
+                            dir="back",
+                            label=ret_label,
+                            color="Green",
+                            headport="c",
+                        )
+                    )
+
+        return dotgraph.to_string()
+
 
 class node_data(object):
-
-    def __init__(self, _args=None, _kwargs=None, _fnname="", _ret=None, _childmethods=[]):
+    def __init__(
+        self, _args=None, _kwargs=None, _fnname="", _ret=None, _childmethods=[]
+    ):
         self.args = _args
         self.kwargs = _kwargs
         self.fn_name = _fnname
@@ -127,18 +153,17 @@ class node_data(object):
 
     def argstr(self):
         s_args = ", ".join([str(arg) for arg in self.args])
-        s_kwargs = ", ".join([(str(k), str(v))
-                             for (k, v) in self.kwargs.items()])
+        s_kwargs = ", ".join([(str(k), str(v)) for (k, v) in self.kwargs.items()])
         return "%s%s" % (s_args, s_kwargs)
 
 
 class viz(object):
-    ''' decorator to construct the call graph with args and return values as labels '''
+    """ decorator to construct the call graph with args and return values as labels """
 
     def __init__(self, wrapped):
         self.wrapped = wrapped
-        self.max_frames = 1000 
-        self.max_time = 10 # in seconds
+        self.max_frames = 1000
+        self.max_time = 10  # in seconds
         self.start_time = time()
 
     def __call__(self, *args, **kwargs):
@@ -152,15 +177,16 @@ class viz(object):
         fullstack = inspect.stack()
 
         this_frame_id = id(fullstack[0][0])
-        if fullstack[2].function == '__call__':
-          caller_frame_id = id(fullstack[2][0])
+        if fullstack[2].function == "__call__":
+            caller_frame_id = id(fullstack[2][0])
 
         if this_frame_id not in g_frames:
             g_frames.append(fullstack[0][0])
 
         if this_frame_id not in g_callers.keys():
             g_callers[this_frame_id] = node_data(
-                args, kwargs, self.wrapped.__name__, None, [])
+                args, kwargs, self.wrapped.__name__, None, []
+            )
 
         edgeinfo = None
         if caller_frame_id:
@@ -169,10 +195,14 @@ class viz(object):
             callgraph.increment()
 
         if len(g_frames) > self.max_frames:
-          raise TooManyFramesError(f"Encountered more than ${self.max_frames} while executing function")
+            raise TooManyFramesError(
+                f"Encountered more than ${self.max_frames} while executing function"
+            )
         if (time() - self.start_time) > self.max_time:
-          raise TooMuchTimeError(f"Took more than ${self.max_time} seconds to run function")
-          
+            raise TooMuchTimeError(
+                f"Took more than ${self.max_time} seconds to run function"
+            )
+
         # Invoke the wrapped
         ret = self.wrapped(*args, **kwargs)
 
@@ -197,9 +227,9 @@ def decorate_funcs(func_source: str):
 
 
 def visualize(function_definition, function_call):
-  """Either returns generated SVG or generates an error."""
-  callgraph.reset()
-  function_definition=decorate_funcs(function_definition)
-  exec(function_definition, globals())
-  eval(function_call)
-  return callgraph.render()
+    """Either returns generated SVG or generates an error."""
+    callgraph.reset()
+    function_definition = decorate_funcs(function_definition)
+    exec(function_definition, globals())
+    eval(function_call)
+    return callgraph.render()
