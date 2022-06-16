a06f08512e6df6e0300fd9639dc1dd376affbb8b
Pamela Fox <pamela.fox@gmail.com>
2022-06-16 14:42:45 -0700

    Fix for mutable args

--- rcviz_test.py
+++ rcviz_test.py
@@ -5,7 +5,6 @@ from rcviz import visualize, TooManyFramesError, TooMuchTimeError
 class TestRCViz(unittest.TestCase):
 
   fib_def = """
-@viz
 def fib(n):
   if n == 0:
     return 0
@@ -15,8 +14,21 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)"""
 
   def test_svg(self):
-    svg = visualize(self.fib_def, 'fib(2)')
-    self.assertIn('fib(1)', svg)
+    dotgraph = visualize(self.fib_def, 'fib(2)')
+    self.assertIn('fib(1)', dotgraph)
+
+  def test_mutable_args(self):
+    rev_def = """
+def rev(lst, start, end):
+  if start < end:
+    tmp = lst[end]
+    lst[end] = lst[start]
+    lst[start] = tmp
+    rev(lst, start+1, end-1)
+"""
+    dotgraph = visualize(rev_def, 'rev([1, 2, 3, 4, 5], 0, 4)')
+    self.assertIn('rev([5, 2, 3, 4, 1], 1, 3)', dotgraph)
+    self.assertIn('rev([5, 4, 3, 2, 1], 2, 2)', dotgraph)
 
   def test_error_too_many_nodes(self):
     with self.assertRaises(TooManyFramesError):
@@ -24,13 +36,14 @@ def fib(n):
 
   def test_error_too_much_time(self):
     while_def = """
-@viz
 def inf():
   while True:
     inf()
 """
     with self.assertRaises(TooMuchTimeError):
       visualize(while_def, 'inf()')
+
+
 
 if __name__ == '__main__':
     unittest.main()
