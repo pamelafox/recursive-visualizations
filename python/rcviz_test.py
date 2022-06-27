2679a560694d3ff9b3be6b5e91e9c51c70b90dcf
Pamela Fox <pamela.fox@gmail.com>
2022-06-27 12:56:45 -0700

    Migrated to pytest

--- python/rcviz_test.py
+++ python/rcviz_test.py
@@ -1,11 +1,9 @@
-import unittest
+import pytest
 
-from rcviz import visualize, TooManyFramesError, TooMuchTimeError
+import rcviz
 
 
-class TestRCViz(unittest.TestCase):
-
-    fib_def = """
+fib_def = """
 def fib(n):
   if n == 0:
     return 0
@@ -14,12 +12,33 @@ def fib(n):
   else:
     return fib(n - 1) + fib(n - 2)"""
 
-    def test_svg(self):
-        dotgraph = visualize(self.fib_def, "fib(2)")
-        self.assertIn("fib(1)", dotgraph)
 
-    def test_mutable_args(self):
-        rev_def = """
+def test_visualize():
+    dotgraph = rcviz.visualize(fib_def, "fib(2)")
+    assert "fib(1)" in dotgraph
+
+
+def test_error_too_many_nodes(monkeypatch):
+    monkeypatch.setattr(rcviz, "MAX_FRAMES", 10)
+
+    with pytest.raises(rcviz.TooManyFramesError):
+        rcviz.visualize(fib_def, "fib(14)")
+
+
+def test_error_too_much_time(monkeypatch):
+    while_def = """
+def inf():
+  while True:
+    inf()
+"""
+    monkeypatch.setattr(rcviz, "MAX_TIME", 2)
+
+    with pytest.raises(rcviz.TooMuchTimeError):
+        rcviz.visualize(while_def, "inf()")
+
+
+def test_mutable_args():
+    rev_def = """
 def rev(lst, start, end):
   if start < end:
     tmp = lst[end]
@@ -27,23 +46,6 @@ def rev(lst, start, end):
     lst[start] = tmp
     rev(lst, start+1, end-1)
 """
-        dotgraph = visualize(rev_def, "rev([1, 2, 3, 4, 5], 0, 4)")
-        self.assertIn("rev([5, 2, 3, 4, 1], 1, 3)", dotgraph)
-        self.assertIn("rev([5, 4, 3, 2, 1], 2, 2)", dotgraph)
-
-    def test_error_too_many_nodes(self):
-        with self.assertRaises(TooManyFramesError):
-            visualize(self.fib_def, "fib(14)")
-
-    def test_error_too_much_time(self):
-        while_def = """
-def inf():
-  while True:
-    inf()
-"""
-        with self.assertRaises(TooMuchTimeError):
-            visualize(while_def, "inf()")
-
-
-if __name__ == "__main__":
-    unittest.main()
+    dotgraph = rcviz.visualize(rev_def, "rev([1, 2, 3, 4, 5], 0, 4)")
+    assert "rev([5, 2, 3, 4, 1], 1, 3)" in dotgraph
+    assert "rev([5, 4, 3, 2, 1], 2, 2)" in dotgraph
